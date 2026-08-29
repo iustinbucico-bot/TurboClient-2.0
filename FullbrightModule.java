426d36322254d5b880770f7f45b9989f7a86f0e8
TurboClient <iustinbucico@gmail.com>
2026-08-29 09:52:46 +0300

    Add FullbrightModule to adjust gamma settings

---
+++ FullbrightModule.java
@@ -0,0 +1,17 @@
+package com.yourname.client.modules;
+
+import net.minecraft.client.MinecraftClient;
+
+public class FullbrightModule extends Module {
+    public FullbrightModule() {
+        super("Fullbright");
+    }
+    @Override
+    public void onEnable() {
+        MinecraftClient.getInstance().options.getGamma().setValue(100.0);
+    }
+    @Override
+    public void onDisable() {
+        MinecraftClient.getInstance().options.getGamma().setValue(0.5);
+    }
+}
