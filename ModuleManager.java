c3ef8dd45d20fcbbab39562236c19bf23b44e036
TurboClient <iustinbucico@gmail.com>
2026-08-29 12:27:34 +0300

    Implement ModuleManager for client modules

---
+++ ModuleManager.java
@@ -0,0 +1,24 @@
+package com.yourname.client;
+
+import com.yourname.client.modules.*;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModuleManager {
+    public static List<Module> modules = new ArrayList<>();
+
+    public static void init() {
+        modules.add(new FullbrightModule());
+        modules.add(new SprintModule());
+        modules.add(new KillauraModule());
+        modules.add(new TargetStrafeModule());
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            for (Module m : modules) {
+                if (m.enabled) m.onTick();
+            }
+        });
+    }
+}
